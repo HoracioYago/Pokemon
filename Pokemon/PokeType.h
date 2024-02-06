8ed7d202b14f9b6865ca2ce9ee067e999bd6fe13
bludg0d <mathias.lust@artfx.fr>
2024-02-06 12:41:22 +0100

    [Fix] types

--- Pokemon/PokeType.h
+++ Pokemon/PokeType.h
@@ -1,7 +1,31 @@
 #pragma once
 enum class PokeType
 {
-	//everytype
+	Normal;
+	Fire;
+	Water;
+	Grass;
+	Electric;
+	Ice;
+	Fighting;
+	Poison;
+	Ground;
+	Flying;
+	Psychic;
+	Bug;
+	Rock;
+	Ghost;
+	Dragon;
+	Dark;
+	Steel;
+	Fairy;
 };
 
+//Fire: is weak to
+// is strong to 
+
+
+
 //then to change string to enum, use an int X to compare to i within enum
+//multiply type weakness by 2 resistance by .5
+//create a method 
